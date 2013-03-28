eac9cc68fb4963b9a53b83721bfe002369742c06
twintaka <rashearth@gmail.com>
2013-03-28 21:23:09 +0900

    Update MainActivity.java

--- shaka2/src/jp/hackathony/shaka2/MainActivity.java
+++ shaka2/src/jp/hackathony/shaka2/MainActivity.java
@@ -5,7 +5,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import jp.hackathony.shaka2.AcceleratorClass;
-
+import jp.hackathony.shaka2.GetAcceleratorClass;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -16,65 +16,27 @@ import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity implements SensorEventListener {
-    private SensorManager manager;
-	public Timer timer = null; 
-	int tickcount;
-	double x,y,z;
-	AcceleratorClass AccObj=new AcceleratorClass();
-	public void _Timer(){
-		tickcount=0;
-		if(timer!=null){
-			timer.cancel();
-			timer=null;
-		}
-		timer = new Timer(true);
-		timer.schedule(			
-				new TimerTask() {
-					@Override
-					public void run() {
-						AccObj.x.add(x);
-						AccObj.y.add(y);
-						AccObj.z.add(z);
-						tickcount++;
-						if(tickcount>AccObj.KANKAKUCOUNT){
-							SendAccelerator();
-							timer.cancel();
-							
-						}
-						System.out.println("x="+x+",y="+y+",z="+z);
-					}
-				}, 10, 10
-
-		);
-	}
+	private SensorManager manager;
+	GetAcceleratorClass getAcc=new GetAcceleratorClass();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         manager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        this._Timer();
+        getAcc.Create(manager);
+        Log.d("kanakanakana","waaaa");
     }
 
     @Override
     protected void onResume() {
     	super.onResume();
-    	List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ACCELEROMETER);
-    	if(sensors.size() > 0) {
-    		Sensor s = sensors.get(0);
-    		manager.registerListener(this, s, SensorManager.SENSOR_DELAY_UI);
-    	}
+    	getAcc.Resume();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
- 
-    
-    void SendAccelerator(){
-    	//ここに入れる。
-    	System.out.println("kanachanha"+forKana(AccObj));
     }
 
 
@@ -84,41 +46,9 @@ public class MainActivity extends Activity implements SensorEventListener {
 		
 	}
 
-
 	@Override
-	public void onSensorChanged(SensorEvent event) {
+	public void onSensorChanged(SensorEvent arg0) {
 		// TODO Auto-generated method stub
-		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-			x=(double) event.values[0];
-			y=(double) event.values[1];
-			z=(double) event.values[2];
-		}
-	}
-	public int forKana(AcceleratorClass kana){
-		int i;
-		double sum_x=0.0, sum_y=0.0, sum_z=0.0;
-		for(i=0;i<kana.x.size()-1;i++){
-			sum_x += Math.pow(kana.x.get(i+1)-kana.x.get(i),2);
-		}
-		for(i=0;i<kana.x.size()-1;i++){
-			sum_y += Math.pow(kana.y.get(i+1)-kana.y.get(i),2);
-		}
-		for(i=0;i<kana.x.size()-1;i++){
-			sum_z += Math.pow(kana.z.get(i+1)-kana.z.get(i),2);
-		}
-		Log.d("kanakanakana",sum_x + ","+ sum_y +","+ sum_z);
-		if(sum_x>sum_y){
-			if(sum_x>sum_z){
-				return 1;
-			}else{
-				return 3;
-			}
-		}else{
-			if(sum_y>sum_z){
-				return  2;
-			}else{
-				return 3;
-			}
-		}
+		
 	}
 }
